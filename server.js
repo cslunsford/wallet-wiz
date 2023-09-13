f7aeb37106f22828a330111a541fff04946c91aa
Compass <daniktherrien@gmail.com>
2023-09-12 21:32:01 -0400

    working server and working connection to plaid sandbox. Step1 of create link token is working

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 //store environment variables in .env without exposing them in the code
 require('dotenv').config();
 
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 // Import Express and create an Express app
 const express = require("express");
@@ -11,56 +13,56 @@ app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
-// Serve static files from the 'public' directory
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 
-
+//Axios
+const axios = require('axios');
 
 
 // Import Plaid SDK https://www.npmjs.com/package/plaid //
-const  { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
+const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
 // Import Plaid credentials from .env file
 const PLAIDCLIENT_ID = process.env.PLAIDCLIENT_ID;
 const PLAIDSECRET = process.env.PLAIDSECRET;
 // Create a new instance of the Plaid client
-const plaidclient = new PlaidApi(Configuration);
+
 
 
 // Initialize the Plaid client
 const configuration = new Configuration({
   basePath: PlaidEnvironments.sandbox,
   baseOptions: {
-    headers: {
-      'PLAID-CLIENT-ID': PLAIDCLIENT_ID,
-      'PLAID-SECRET': PLAIDSECRET,
-    },
+      headers: {
+          'PLAID-CLIENT-ID': PLAIDCLIENT_ID,
+          'PLAID-SECRET': PLAIDSECRET,
+      },
   },
 });
+//this has to come after the configuration / plaid client init
+const plaidClient = new PlaidApi(configuration);
 
 // Create a new link_token from PLAID -- Step 1
 app.post('/create_link_token', async function (request, response) {
-  // Get the client_user_id by searching for the current user
-  // const user = await User.find(...);
-  // const clientUserId = user.id;
-  const plaidrequest = {
-    user: {
-      // This should correspond to a unique id for the current user.
-      //***************THIS USER ID COMES FROM THE MYSQL DB ********//
-      client_user_id: "user",
-    },
-    client_name: 'Plaid Test App',
-    products: ['auth'],
-    language: 'en',
-    redirect_uri: 'http://localhost:3000/',
-    country_codes: ['US'],
+  const plaidRequest = {
+      user: {
+          client_user_id: 'user',
+      },
+      client_name: 'Plaid Test App',
+      products: ['auth'],
+      language: 'en',
+      redirect_uri: 'http://localhost:3000/',
+      country_codes: ['US'],
   };
   try {
-    const createTokenResponse = await plaidclient.linkTokenCreate(plaidrequest);
-    response.json(createTokenResponse.data);
+      const createTokenResponse = await plaidClient.linkTokenCreate(plaidRequest);
+      response.json(createTokenResponse.data);
   } catch (error) {
-    // handle error message
-    response.status(500).send("linkTokenCreate failed");
+      response.status(500).send("failure");
+      console.error('Plaid API Error:', error.response ? error.response.data : error.message);
+      // handle error
   }
 });
 
@@ -68,15 +70,50 @@ app.post('/create_link_token', async function (request, response) {
 
 
 
+app.post("/auth", async function(request, response) {
+  try {
+      const access_token = request.body.access_token;
+      const plaidRequest = {
+          access_token: access_token,
+      };
+      const plaidResponse = await plaidClient.authGet(plaidRequest);
+      response.json(plaidResponse.data);
+  } catch (e) {
+      response.status(500).send("failed");
+  }
+});
+
+app.post('/exchange_public_token', async function (
+  request,
+  response,
+  next,
+) {
+  const publicToken = request.body.public_token;
+  try {
+      const plaidResponse = await plaidClient.itemPublicTokenExchange({
+          public_token: publicToken,
+      });
+      // These values should be saved to a persistent database and
+      // associated with the currently signed-in user
+      const accessToken = plaidResponse.data.access_token;
+      response.json({ accessToken });
+  } catch (error) {
+      response.status(500).send("failed");
+  }
+});
 
 
 
+async function fetchData() {
+  try {
+    const response = await axios.post('http://localhost:3000/create_link_token');
+    console.log('Response:', response.data);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
 
-
-
-
-
-
+fetchData();
 
 
 
