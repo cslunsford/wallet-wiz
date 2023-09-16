5fa28eeb6c26e1d92f62c8170bab5227f5a00529
cslunsford <christopher.lunsford@uconn.edu>
2023-09-15 22:31:11 -0400

    added homepage and dashboard nav functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,10 @@ router.get('/', (req, res) => {
     res.render('homepage');
 });
 
+router.get('/homepage', (req, res) => {
+    res.render('homepage');
+})
+
 router.get('/dashboard', (req, res) => {
     res.render('dashboard');
 });
