f2e5465549e9fccb71f71fa2dcbbeba637bc1d94
Compass <daniktherrien@gmail.com>
2023-09-19 16:11:48 -0400

    added the procfile and changed port to 8000

--- server.js
+++ server.js
@@ -48,7 +48,8 @@ app.use(express.json());
 app.use(userRoutes);
 app.use(authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
@@ -173,7 +174,7 @@ app.get('/transactionssync', async function (request,response,next) {
           response.status(500).send("not failure");
           console.error('Plaid API Error:', error.response ? error.response.data : error.message);
     }
-  });
+  })
  
   function prettyPrintResponse(data) {
     // Implement the function logic here
